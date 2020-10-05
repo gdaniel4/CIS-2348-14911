39e19c3145ba1fff4ac74b7e9b946e6f30a5b6e0
gdaniel4 <70462084+gdaniel4@users.noreply.github.com>
2020-10-05 07:48:12 -0500

    Section C

--- HW2/CIS 2348 Howework 2.py
+++ HW2/CIS 2348 Howework 2.py
@@ -1,16 +1,42 @@
-#reads dates from input
-input_dates = open(r'C:\Users\gabed\OneDrive\Documents\Gabriel Daniels\inputDates.txt')
-
-
 month_list = {'January': '1', 'February': '2','March': '3','April': '4','May': '5','June': '6','July': '7','August': '8','September': '9','October': '10','November': '11', 'December': '12'}
-enter_date = input("Format date as Month Day, Year (January 1, 1999): ")
+input_dates = open(r'C:\Users\gabed\OneDrive\Documents\Gabriel Daniels\inputDates.txt')
+output_dates = open('C:\\Users\\gabed\\OneDrive\\Documents\\Gabriel Daniels\\outputDates.txt','w')
+
+#reads dates from input
+"""enter_date = input("Format date as Month Day, Year (January 1, 1999): ")
 enter_date = enter_date.replace(',','')
-enter_date = enter_date.split(' ')
+enter_date = enter_date.split(' ')"""
+
 #gets list made by .split and replaces month with interger using dictionary above
-month = enter_date[0]
+"""month = enter_date[0]
 month = month_list[month]
 enter_date[0] = month
 #rejoins the date with all intergers to match formatting
 date = '/'.join(enter_date)
-print(date)
+print(date)"""
+
+#Parses input dates into correct format
+for date in input_dates:
+    if '/' in date:
+        continue
+    elif '.' in date:
+        continue
+    elif '-' in date:
+        continue
+    elif date != "-1":
+        # reads dates from input
+        date = date.replace(',','')
+        date = date.split(' ')
+        # gets list made by .split and replaces month with interger using dictionary above
+        month = date[0]
+        month = month_list[month]
+        date[0] = month
+        # rejoins the date with all intergers to match formatting
+        date = '/'.join(date)
+        #puts all properly formatted dates into list
+        output_dates.write(date)
+        print(date)
+
+output_dates.close()
+input_dates.close()
 
